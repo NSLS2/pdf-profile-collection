6090f993cea6d8239e6283b322c9b313951f3ee5
PDF Operator <xf28id1@xf28id1-ws2.nsls2.bnl.local>
2025-09-16 15:11:08 -0400

    reverse fs and add temperature pbar @clin1

--- startup/11-temperature-controller.py
+++ startup/11-temperature-controller.py
@@ -419,3 +419,175 @@ hotairblower=Eurotherm('XF:28ID1-ES:1{Env:05}LOOP1:PV:RBV',
 #        tolerance=1,name='hotairblower')
 
 sorensen850_manual = EpicsSignal('XF:28ID1-ES{LS336:1-Out:3}Out:Man-RB', write_pv='XF:28ID1-ES{LS336:1-Out:3}Out:Man-SP', name='sorensen850_manual')
+
+
+from ophyd.status import SubscriptionStatus, MoveStatus
+
+
+class Lakeshore336Setpoint2(PVPositioner):
+    readback = Cpt(EpicsSignalRO, 'T-RB')
+    setpoint = Cpt(EpicsSignal, 'T-SP')
+    done = Cpt(EpicsSignalRO, 'Sts:Ramp-Sts')
+    ramp_enabled = Cpt(EpicsSignal, 'Enbl:Ramp-Sel')
+    ramp_rate = Cpt(EpicsSignal, 'Val:Ramp-SP')
+    p_gain = Cpt(EpicsSignal, 'Gain:P-RB', write_pv='Gain:P-SP')
+    i_gain = Cpt(EpicsSignal, 'Gain:I-RB', write_pv='Gain:I-SP')
+    d_gain = Cpt(EpicsSignal, 'Gain:D-RB', write_pv='Gain:D-SP')
+    done_value = 0
+
+    set_max_val = Cpt(EpicsSignal, 'OUT:Max-SP')
+    read_max_val = Cpt(EpicsSignalRO, 'OUT:Max-RB')
+
+    # status = Cpt(EpicsSignalRO, 'Enbl-Sts')
+    range_selection = Cpt(EpicsSignal, 'Val:Range-Sel')
+    range_status = Cpt(EpicsSignalRO, 'Val:Range-Sts')
+
+    def __init__(self, *args, setpoint_tolerance=1, **kwargs):
+
+        # Sets tolerance for skipping `set` calls when already at setpoint.
+        self.setpoint_tolerance = setpoint_tolerance
+        super().__init__(*args, **kwargs)
+
+
+    def set_setpoint(self, new_position, *args, timeout=None, wait=True, **kwargs):
+
+        # Check if new setpoint is within setpoint_tolerance of setpoint redback, and if the controller is
+        # at it's setpoint. If it is, return that the move was successful instantly to avoid lockup.
+        if abs(self.readback.get() - new_position) < self.setpoint_tolerance:
+            return DeviceStatus(self, success=True, done=True)
+        else:
+            return super().set(new_position, *args, timeout=timeout, wait=wait, **kwargs)
+
+
+    def select_range(self, new_range, *args, timeout=None, wait=True, **kwargs):
+
+        # Check if range_selection is same as range_status, and if the controller is
+        # at it's setpoint. If it is, return that the move was successful instantly to avoid lockup.
+        if self.range_status.get() == new_range:
+            return DeviceStatus(self, success=True, done=True)
+        else:
+            return super().set(new_range, *args, timeout=timeout, wait=wait, **kwargs)
+
+
+
+
+def temperature_pbar(start_T, stop_T, T_callable, status):
+    from tqdm import tqdm
+    with tqdm(total=100, desc=f'Target: {T_callable.get()}/{stop_T} K') as pbar:
+        percent_T = 0
+        while not status.success:
+            status.check_value(new_value=stop_T, old_value=T_callable.get())
+            percent_T = 100*abs((T_callable.get()-start_T)/(stop_T-start_T)) - percent_T
+            pbar.set_description(f'Target: {T_callable.get()}/{stop_T} K')
+            pbar.update(percent_T)
+            time.sleep(2)
+        
+        print('\n################## Reach Target #######################\n')
+
+    return status
+
+
+
+class Lakeshore336_2(Device):
+    temp = DDC(_temp_fields(['A','B','C','D']))
+    out1 = Cpt(Lakeshore336Setpoint2, '-Out:1}')
+    out2 = Cpt(Lakeshore336Setpoint2, '-Out:2}')
+    out3 = Cpt(Lakeshore336Setpoint2, '-Out:3}')
+    out4 = Cpt(Lakeshore336Setpoint2, '-Out:4}')
+
+    def __init__(self, *args, tolerance=0.5, **kwargs):
+        self.tolerance = tolerance
+        super().__init__(*args, **kwargs)
+
+    def set_and_check(self, new_value, new_range, input_channel='temp.A', output_channel='out1'):
+        out_obj = getattr(self, output_channel)
+        in_obj = getattr(self, input_channel)
+        T_from_sensor = in_obj.T
+        start_T = T_from_sensor.get()
+        
+        def check_setpoint(*, new_value, old_value, **kwargs):
+            if abs(new_value - T_from_sensor.get()) < self.tolerance:
+                print(f'Reached setpoint {T_from_sensor.get()}.')
+                return True
+            return False
+
+        print(f'\nSet {output_channel = } to temperature = {new_value} K, using channel_range = {new_range}\n')
+        out_obj.select_range(new_range, wait=False)
+        out_obj.set_setpoint(new_value, wait=False)
+        status = SubscriptionStatus(T_from_sensor, run=True, callback=check_setpoint)
+
+        status = temperature_pbar(start_T, out_obj.setpoint.get()-self.tolerance, T_from_sensor, status)
+
+        # while not status.success:
+        #     status.check_value(new_value=out_obj.setpoint.get(), old_value=T_from_sensor.get())
+
+        #     if status.success is False:
+        #         print(f'{T_from_sensor.name} = {T_from_sensor.get():.2f} K, not reach target')
+        #         time.sleep(5)
+        #     else:
+        #         print('################## Reach Target #######################')
+            
+
+        return status
+
+
+
+lakeshore336_2 = Lakeshore336_2('XF:28ID1-ES{LS336:1' , name='lakeshore336_2')
+
+def check_setpoint(new_value, old_value, **kwargs):
+    if abs(new_value - T_from_sensor.get()) < tolerance:
+        print(f'Reached setpoint {T_from_sensor.get()}.')
+        return True
+    return False
+
+import bluesky.plan_stubs as bps
+def cryostat_set_temp(new_temp, new_range, input_channel='temp.A', output_channel='out1', temp_controller=lakeshore336, tolerance=0.5):
+    out_obj = getattr(temp_controller, output_channel)
+    in_obj = getattr(temp_controller, input_channel)    
+    T_from_sensor = in_obj.T
+    start_T = T_from_sensor.get()
+
+    # def check_setpoint(new_value, old_value, **kwargs):
+    #     if abs(new_value - T_from_sensor.get()) < tolerance:
+    #         print(f'Reached setpoint {T_from_sensor.get()}.')
+    #         return True
+    #     return False
+    
+    print(f'\nSet {output_channel = } to temperature = {new_temp} K, using channel_range = {new_range}\n')
+    yield from bps.mv(out_obj.range_selection, new_range, out_obj.setpoint, new_temp)
+    
+    status = SubscriptionStatus(T_from_sensor, run=True, callback=check_setpoint(T_from_sensor=T_from_sensor))
+
+    status = temperature_pbar(start_T, out_obj.setpoint.get()-tolerance, T_from_sensor, status)
+        
+    return status
+
+    
+
+def pbaar_set_temp(new_temp, 
+                   device_config = {
+                       'temp_controller':lakeshore336, 
+                       'range':3, 
+                       'input_channel':'temp.A', 
+                       'output_channel':'out1',}, 
+                   tolerance=0.5):
+    
+    out_obj = getattr(temp_controller, output_channel)
+    in_obj = getattr(temp_controller, input_channel)    
+    T_from_sensor = in_obj.T
+    start_T = T_from_sensor.get()
+
+    # def check_setpoint(new_value, old_value, **kwargs):
+    #     if abs(new_value - T_from_sensor.get()) < tolerance:
+    #         print(f'Reached setpoint {T_from_sensor.get()}.')
+    #         return True
+    #     return False
+    
+    print(f'\nSet {output_channel = } to temperature = {new_temp} K, using channel_range = {new_range}\n')
+    yield from bps.mv(out_obj.range_selection, new_range, out_obj.setpoint, new_temp)
+    
+    status = SubscriptionStatus(T_from_sensor, run=True, callback=check_setpoint(T_from_sensor=T_from_sensor))
+
+    status = temperature_pbar(start_T, out_obj.setpoint.get()-tolerance, T_from_sensor, status)
+        
+    return status
